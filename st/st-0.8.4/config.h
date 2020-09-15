3649e30e8bda7992e0d3f6b5017eeb9e72e356d3
Kacper Uminski <kacperum@gmail.com>
2020-09-15 22:11:30 +0200

    Minor edit

--- st/st-0.8.4/config.h
+++ st/st-0.8.4/config.h
@@ -188,7 +188,7 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-/*#define MODKEY Mod1Mask*/
+#define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
